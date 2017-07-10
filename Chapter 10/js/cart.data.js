7369e8795745dfec38f575dcf0b0c90b95d173c8
sonicpro <sonic@ukrpost.net>
2017-07-10 11:05:38 +0300

    Initial commit when switching to Github from VS online.

---
+++ Chapter 10/js/cart.data.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+(function (f, define) {
+    define("cart.data", ["kendo.data", "jquery/jquery"], f);
+})(
+    // This funcition is executed by require.js.
+    function () {
+        "use strict";
+        $.extend(window, {
+            cart: {
+                kendoDS: function (url, options) {
+                    var settings = $.extend({
+                        filter: null
+                    }, options || {});
+                    return new kendo.data.DataSource({
+                        transport: {
+                            read: url
+                        },
+                        type: "odata",
+                        serverFiltering: true,
+                        filter: settings.filter
+                    });
+                }
+            }
+        });
+    },
+    typeof define === "function" && define.amd ? define : function (_, d, f) {
+        f();
+    });
